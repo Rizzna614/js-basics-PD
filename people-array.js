21bef5bb5ffa783f8fb5417b8a8f9085f37a9b5f
emils <emils@gmail.com>
2025-10-27 11:09:22 +0200

    ceturtais uzd

---
+++ people-array.js
@@ -0,0 +1,14 @@
+let people =
+[
+    Anna = {name: "Anna", age: 25, isStudent: false},
+    Pēteris = {name: "Pēteris", age: 20, isStudent: true},
+];
+
+for (let i = 0; i < people.length; i++)
+{
+         console.log("Vārds: " + people[i].name)
+};
+
+
+
+
