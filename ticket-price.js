6e1af382be06e5d5cb82a5cde8e3935e34a37554
emils <emils@gmail.com>
2025-10-27 10:56:58 +0200

    second uzd

---
+++ ticket-price.js
@@ -0,0 +1,13 @@
+let visitorAge = 12;
+isStudent = true;
+
+if (visitorAge <= 6)
+{
+      console.log("Biļete ir bez maksas")
+} else if (isStudent == true)
+{
+          console.log("Biļetes cena ir 5eiro")
+} else
+{
+         console.log("Biļetes cena ir 10eiro")
+};
